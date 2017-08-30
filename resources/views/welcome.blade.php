a52303181d481371e064489e36dea35eaa13a01e
agadenis <aga.j.denis@gmail.com>
2017-08-30 17:13:39 +0100

    Change welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Another welcome to Laravel 5
+                    This is Laravel 5
                 </div>
 
                 <div class="links">
